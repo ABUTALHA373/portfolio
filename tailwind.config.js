8d90e0bf7f565723dbccdde92b169c41176366e5
Abu Talha <abutalha5896@gmail.com>
2024-10-09 14:25:59 +0600

    tailwind content problem solvel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js}",
+    "./**/*.{html,js}",
+    "./*.html"
+  ],
+
   theme: {
     screens: {
       sm: '641px', // Change the width for the sm breakpoint
